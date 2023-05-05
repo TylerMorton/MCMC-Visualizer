d94fb0c1b419b668e40d41e7725c5beca43d9232
tyler <tbmorton@ucsc.edu>
2023-05-05 01:01:27 -0700

    initial metropolis algorithm, no hastings yet! Excited to get the algorithm running and add some visualization... coming soon.

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use mcmc::{acceptance, gaussian_sample};
+use rand::Rng;
+
+pub fn main() {
+    /* CAUTION these initial project assumes
+     * the PDF provided is proportional to the Gaussian Distribution!!
+     */
+    let mut position = gaussian_sample();
+    let samples: i32 = 20;
+
+    let mut rng = rand::thread_rng();
+    for _ in 0..samples {
+        println!("current position: {}", position);
+        loop {
+            // hardcoded to gaussian for now (these should be custom eventually)
+            let candidate = gaussian_sample();
+            let prob_accept = acceptance(position, candidate);
+            if prob_accept > rng.gen() {
+                println!("candidate: {:.2} was accepted", candidate);
+                position = candidate;
+                break;
+            } else {
+                println!("rejected");
+            }
+        }
+    }
+}
